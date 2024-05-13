241c3f8d67da93b77b3ee15f4851f6e2da9d0a36
Faizurcse <mdfaizurrahman2000@gmail.com>
2024-05-13 18:11:20 +0530

    sdfjdjnajdnjnfj000000999923412211

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin= require('html-webpack-plugin');
 module.exports = {
-  entry: ["regenerator-runtime/runtime.js", "./src/index.js"],
+    entry: './src/index.js',
 
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$|\.jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-          },
+    output: {
+        path: path.join(__dirname,"/dist"),
+        filename: "index_bundle.js",
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js$|\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ]
+            },
         ],
-      },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
 };
